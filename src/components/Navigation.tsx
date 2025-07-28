558599a1983fabd318183ce4557bea1c3707ec0d
jgsilva747 <75501358+jgsilva747@users.noreply.github.com>
2025-07-28 11:52:34 +0100

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -25,7 +25,7 @@ const Navigation = () => {
               className="flex items-center gap-2 focus:outline-none"
             >
               <div className="rounded bg-accent">
-                <img src="/_zero_logo.jpg" alt="Logo Icon" className="w-10 h-10" />
+                <img src="/Zero_Symbol.svg" alt="Logo Icon" className="w-10 h-10" />
               </div>
               <span className="font-bold text-xl">Zero Industries</span>
             </button>
